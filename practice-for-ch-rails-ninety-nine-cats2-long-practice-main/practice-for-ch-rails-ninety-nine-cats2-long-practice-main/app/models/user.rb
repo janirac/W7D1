48bb9b21cb75e91f3b561f7a6af975023a077976
Brandon Choi <bchoi28@gmail.com>
2022-12-19 15:46:26 -0500

    finished is_password? in user model

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -9,8 +9,24 @@ class User < ApplicationRecord
 
     def password=(new_pass)
         self.password_digest = BCrypt::Password.create(new_pass)
-        @password = new_pass #question?
+        @password = new_pass #setting the password to pass the validation
     end 
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user 
+        else 
+            nil 
+        end
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    
 
 
 end
